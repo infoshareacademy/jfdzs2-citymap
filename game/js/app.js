617bd84a15f3b14076ecd35ba41ec531f3ddebb5
JoannaDS <j.dabrowska.slaby@gmail.com>
2018-04-06 13:57:01 +0200

    Adding jquery to the game, chanig covers of the tiles

--- game/js/app.js
+++ game/js/app.js
@@ -1,65 +1,114 @@
-var tilesArray = [`box1`, `box1`, `box2`, `box2`, `box3`, `box3`, `box4`, `box4`, `box5`, `box5`, `box6`, `box6`];
-var tilesIds = [];
-var memory = [];
-var tilesMatched = 0;
+var cart1 = $('#box1');
+var cart2 = $('#box2');
+var cart3 = $('#box3');
+var cart4 = $('#box4');
+var cart5 = $('#box5');
+var cart6 = $('#box6');
+var cart7 = $('#box7');
+var cart8 = $('#box8');
+var cart9 = $('#box9');
+var cart10 = $('#box10');
+var cart11 = $('#box11');
+var cart12 = $('#box12');
+var points = $('#points p:nth-child(1)');
+var move = $('#move');
 
-Array.prototype.shuffle=function(){
-    var i = this.length , j , temporary;
-    while(--i>0){
-        j = Math.floor(Math.random() * (i+1));
-        temporary= this[j];
-        this[j]= this[i];
-        this[i]= temporary;
+
+cart1.click( function() { if(nextClick === true) {reveal(1)};});
+cart2.click( function() { if(nextClick === true) {reveal(2)};});
+cart3.click( function() { if(nextClick === true) {reveal(3)};});
+cart4.click( function() { if(nextClick === true) {reveal(4)};});
+cart5.click( function() { if(nextClick === true) {reveal(5)};});
+cart6.click( function() { if(nextClick === true) {reveal(6)};});
+cart7.click( function() { if(nextClick === true) {reveal(7)};});
+cart8.click( function() { if(nextClick === true) {reveal(8)};});
+cart9.click( function() { if(nextClick === true) {reveal(9)};});
+cart10.click( function() {if(nextClick === true) {reveal(10)};});
+cart11.click( function() { if(nextClick === true) {reveal(11)};});
+cart12.click( function() {if(nextClick === true) {reveal(12)};});
+
+var arrayCart = [1,1,2,2,3,3,4,4,5,5,6,6];
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
     }
+
+    return a;
 }
 
-function newBoard (){
-    tilesMatched=0;
-    var output = '';
-    tilesArray.shuffle();
-    for(var i =0; i<tilesArray.length; i++){
-        output += '<div id="box_'+i+ '" onclick="memoryFlipTile(this,\''+tilesArray[i]+'\')""></div>';
+arrayCart = shuffle(arrayCart);
+
+console.log(arrayCart)
+
+var tabAcceptedAlreadyClick= [true,true,true,true,true,true,true,true,true,true,true,true];
+
+var choosenFirstCart;
+var secondChoose = false;
+var numberFisrtCartVisible;
+var hidesecondcart;
+var pointsPlayer = 0;
+var nextClick = true;
+var timer1;
+var timer2;
+var numbermove = 0;
+function reveal(cart)
+{
+    console.log(tabAcceptedAlreadyClick)
+    if(tabAcceptedAlreadyClick[cart-1] === true) {
+
+        clearTimeout(timer2);
+        clearTimeout(timer1);
+        $('#picture-' + cart).attr('src', 'images/' + arrayCart[cart - 1] + '.png')
+        console.log(cart);
+        console.log(numberFisrtCartVisible);
+        if (secondChoose === true && cart !== numberFisrtCartVisible) {
+
+            numbermove++;
+            move.text('Move :' + numbermove)
+            nextClick = false;
+            if (choosenFirstCart === arrayCart[cart - 1]) {
+                hidesecondcart = cart;
+
+                if (tabAcceptedAlreadyClick[hidesecondcart - 1] === true && tabAcceptedAlreadyClick[numberFisrtCartVisible - 1] === true) {
+
+                    timer1 = setTimeout(hideAcceptedCart, 1000);
+                }
+
+            }
+            else {
+                hidesecondcart = cart;
+                timer2 = setTimeout(hideDontAcceptedCart, 1000);
+            }
+            secondChoose = false;
+        }
+
+        else {
+            secondChoose = true;
+            numberFisrtCartVisible = cart;
+            choosenFirstCart = arrayCart[cart - 1];
+        }
     }
-    document.getElementById('board').innerHTML=output;
+
+
 }
 
+function hideAcceptedCart() {
+    $('#picture-'+hidesecondcart).addClass('hidden-cart');
+    $('#picture-'+numberFisrtCartVisible).addClass('hidden-cart');
+    pointsPlayer ++;
+    points.text('Points : ' + pointsPlayer);
+    tabAcceptedAlreadyClick[hidesecondcart-1] = false;
+    tabAcceptedAlreadyClick[numberFisrtCartVisible-1] = false;
+    nextClick = true;
+}
 
-function memoryFlipTile(tile,val){
-	if(tile.innerHTML == "" && memory.length < 2){
-		tile.style.background = '#FFF';
-		tile.innerHTML = val;
-		if(memory.length == 0){
-			memory.push(val);
-			tilesIds.push(tile.id);
-		} else if(memory.length == 1){
-			memory.push(val);
-			tilesIds.push(tile.id);
-			if(memory[0] == memory[1]){
-				tilesMatched += 2;
-				// Clear both arrays
-				memory = [];
-            	tilesIds = [];
-				// Check to see if the whole board is cleared
-				if(tilesMatched == memory_array.length){
-					alert("Board cleared... generating new board");
-					document.getElementById('board').innerHTML = "";
-					newBoard();
-				}
-			} else {
-				function flip2Back(){
-				    // Flip the 2 tiles back over
-				    var tile_1 = document.getElementById(tilesIds[0]);
-				    var tile_2 = document.getElementById(tilesIds[1]);
-				    tile_1.style.background = 'url(tile_bg.jpg) no-repeat';
-            	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'url(tile_bg.jpg) no-repeat';
-            	    tile_2.innerHTML = "";
-				    // Clear both arrays
-				    memory = [];
-            	    tilesIds = [];
-				}
-				setTimeout(flip2Back, 700);
-			}
-		}
-	}
+function hideDontAcceptedCart() {
+    $('#picture-'+hidesecondcart).attr('src','images/city2.png');
+    $('#picture-'+numberFisrtCartVisible).attr('src','images/city2.png');
+    nextClick = true;
 }
