26d236adcb7f6381cb45c58fc4f7231086894eb7
JoannaDS <j.dabrowska.slaby@gmail.com>
2018-04-06 13:46:19 +0200

    Changing images

--- game/js/app.js
+++ game/js/app.js
@@ -1,5 +1,4 @@
 var tilesArray = [`box1`, `box1`, `box2`, `box2`, `box3`, `box3`, `box4`, `box4`, `box5`, `box5`, `box6`, `box6`];
-var box1 =document.getElementById("box1");
 var tilesIds = [];
 var memory = [];
 var tilesMatched = 0;
